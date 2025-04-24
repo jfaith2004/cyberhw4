e3ff72a77530f995058abf20deb1414b3994b4d2
jfaith2004 <jfaith2004@gmail.com>
2025-04-24 10:39:38 -0500

    restructure repo

---
+++ pages/index.php
@@ -0,0 +1,17 @@
+<?php
+  // index.php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Welcome</title>
+  <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+  <div class="container">
+    <h1>Welcome</h1>
+    <p>Please <a href="login.php">Sign In</a></p>
+  </div>
+</body>
+</html>
